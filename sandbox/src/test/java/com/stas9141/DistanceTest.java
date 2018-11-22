1a194e01af68b89cf3671e5b02445b4aff5c5182
dbrmlsky <dbrmlsky@gmail.com>
2018-11-22 16:32:17 +0200

    Revert "task 2 finished"
    This reverts commit 4129f404cab16989b5db833fd6d684ae96c17ca3.

--- sandbox/src/test/java/com/stas9141/DistanceTest.java
+++ sandbox/src/test/java/com/stas9141/DistanceTest.java
@@ -6,11 +6,10 @@ import org.testng.annotations.Test;
 public class DistanceTest {
 
     @Test
-
     public void testDistance(){
-        Point p1 = new Point(2.0,2.0);
-        Point p2 = new Point(2.0,2.0);
-        Assert.assertEquals(p1.distance(p2),0.0);
+        Point p1 = new Point(1.0,1.0);
+        Point p2 = new Point(1.0,1.0);
+        Assert.assertEquals(p1.distance(p1,p2),0.0);
         }
         
 }
