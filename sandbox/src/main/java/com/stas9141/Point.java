4129f404cab16989b5db833fd6d684ae96c17ca3
dbrmlsky <dbrmlsky@gmail.com>
2018-11-22 16:30:19 +0200

    task 2 finished

--- sandbox/src/main/java/com/stas9141/Point.java
+++ sandbox/src/main/java/com/stas9141/Point.java
@@ -6,17 +6,13 @@ public class Point {
 
 
     //public  Point(){}; //(empty constructor)
-   public Point(double x, double y) {
-     this.x = x;
-     this.y = y;
-   }
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
 
-    public static double distance(Point p1, Point p2)
-    {
-        double result = Math.sqrt(Math.pow((p2.x-p1.x),2)+Math.pow((p2.y-p1.y),2));
-        System.out.println("distance d between 2 points: "+ "\n"
-                + "x1 = " + p1.x +"; y1 = " +p1.y + "; and x2 = "+ p2.x +"; y2 = "+p2.y +";" +" "
-                + "\n"+ "d = "+ result);
+    public double distance(Point p2) {
+        double result = Math.sqrt(Math.pow((p2.x - this.x), 2) + Math.pow((p2.y - this.y), 2));
         return result;
     }
 
