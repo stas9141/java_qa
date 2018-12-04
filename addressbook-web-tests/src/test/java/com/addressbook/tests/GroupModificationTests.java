3dc51096dde0405f6be172171ff1a377a5ed5bce
Stas9141 <dbrmlsky@gmail.com>
2018-12-04 22:33:54 +0200

    выполнение предусловий для удаления и модификации групп

--- addressbook-web-tests/src/test/java/com/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/com/addressbook/tests/GroupModificationTests.java
@@ -8,6 +8,10 @@ public class GroupModificationTests extends TestBase {
     @Test
     public void testGroupModification(){
         app.getNavigationHelper().gotoGroupPage();
+        
+        if(!app.getGroupHelper().isThereAGroup()){  ///method proverki nalichiya elementa (group)
+            app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
+        }
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
