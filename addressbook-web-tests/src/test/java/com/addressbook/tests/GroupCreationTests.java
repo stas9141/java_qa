0ddd318206ee79d659c138314fbece90b7f16904
Stas9141 <dbrmlsky@gmail.com>
2018-12-13 22:19:07 +0200

    lambda (vichislenie max id dlya grioup creation)

--- addressbook-web-tests/src/test/java/com/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/com/addressbook/tests/GroupCreationTests.java
@@ -5,6 +5,7 @@ import com.addressbook.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -19,7 +20,7 @@ public class GroupCreationTests extends TestBase {
         List<GroupData> before = app.getGroupHelper().getGroupList();
 
         //videlyem peremennuyu
-        GroupData group = new GroupData("test1", "null", "null");
+        GroupData group = new GroupData("test2", "null", "null");
         app.getGroupHelper().createGroup(group);
 
         //spisok group after dobavleniya
@@ -28,17 +29,18 @@ public class GroupCreationTests extends TestBase {
         Assert.assertEquals(after.size(), before.size() + 1);
 
         //sredi all elements from the list, find with maxId - ojidaemiy id new group
-
         int max = 0;
-        for(GroupData g : after){
-            if(g.getId()>max){
+        for (GroupData g : after) {
+            if (g.getId() > max) {
                 max = g.getId();
             }
         }
-        group.setId(max);
+
+        //lambda function dlya vichisleniya max id
+        group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         //dobavlyaem group kotoruyu sozdali v app
         before.add(group);
-        Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
     }
 
